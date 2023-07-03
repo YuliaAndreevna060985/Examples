52721599044ff5213296aad8de5c44a49bcf2fc8
Julia <www.eremenko1985@mail.ru>
2023-07-03 15:01:59 +0300

    Examples003_sum

--- Examples003_sum/Program.cs
+++ Examples003_sum/Program.cs
@@ -1,5 +1,15 @@
-﻿// Написать программу, которя складывает два числа.
+﻿//1. Написать программу, которя складывает два числа.
 
-int numberA = 3;
-int numberB = 5;
-Console.WriteLine(numberA + numberB);
+// int numberA = 3;
+// int numberB = 5;
+// Console.WriteLine(numberA + numberB);
+
+
+// 2. Случайные числа.
+
+int numberA = new Random(). Next (1, 10);
+Console.WriteLine (numberA);
+int numberB = new Random(). Next (1, 10);
+Console.WriteLine (numberB);
+int result = numberA + numberB;
+Console.WriteLine (result);
