4a56a9abb67b92958d60b52f2b1706051d274d01
Julia <www.eremenko1985@mail.ru>
2023-07-04 22:49:18 +0300

    Example006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа для гирь. Найдем max.
+
+int a = 200;
+int b = 4;
+int c = 6;
+int d = 80;
+int e = 10;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write ("max = ");
+Console.WriteLine (max);
